0c308978f2a8d22132e79b6ad04479bb1fb9be68
anonylouisVM <louis.calvie@gmail.com>
2022-10-11 18:43:08 +0200

    todo : bugs on mariadb containers

--- srcs/requirements/wordpress/conf/config_wordpress.sh
+++ srcs/requirements/wordpress/conf/config_wordpress.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export PATH=$PATH:/usr/local/mysql/bin
+
 wp config create --allow-root --path='/var/www/wordpress' --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306
+
+
 php-fpm7.3 -F
