eea3b8ac3a5f5a9c425b0a776353b445f06a040a
anonylouisVM <louis.calvie@gmail.com>
2022-11-11 19:01:55 +0100

    ftp server works

--- srcs/bonus/FTP/conf/config_ftp.sh
+++ srcs/bonus/FTP/conf/config_ftp.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-bash
+if [ -f /is_configure ]
+then
+	echo "FTP already installed"
+	sleep 20
+else
+	sleep 60
+	echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser --gecos "" $FTP_USER
+	echo "$FTP_USER" > /etc/vsftpd.chroot_list
+	chown -R $FTP_USER /var/www/html
+	touch /is_configure
+fi
+
+echo "ftp ready"
+vsftpd /etc/vsftpd.conf
 
